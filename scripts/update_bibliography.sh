72331bdf9e24c2b40089484090e139d75849b247
Matt Heffron <heffron@alumni.caltech.edu>
2023-04-18 20:30:03 -0700

    Fix issue with update_bibliography.sh dropping some entries (#75)
    * Fix issue with update_bibliography.sh dropping some entries at the final stage of json manipulation.
    
    * My first attempt gave the JSON in the wrong structure.
    This seems to be the same "shape" as the original, but doesn't drop entries.
    There probably is an elegant way to accomplish this, but sometimes brute force is just easier.

--- scripts/update_bibliography.sh
+++ scripts/update_bibliography.sh
@@ -31,12 +31,24 @@ add_items_from_collection $root_collection
 
 echo "Got $(jq '. | length' <<< "$items") items"
 
-items=$(jq '
-    sort_by(.data.date)
-    | map(.csljson)
-    | group_by(.issued."date-parts"[0][0])
-    | map({ (.[0].issued."date-parts"[0][0] // "Undated" | tostring): . })
-    | add' <<< "$items")
+# Piece-wise processing for debugging:
+items=$(jq 'sort_by(.data.date)' <<< "$items")
+# echo "$items" > sorted.json
+items=$(jq 'map(.csljson)' <<< "$items")
+# echo "$items" > mapped.json
+items=$(jq 'group_by(.issued."date-parts"[0][0])' <<< "$items")
+# echo "$items" > grouped.json
+items=$(jq 'map({ (.[0].issued."date-parts"[0][0] // "Undated" | tostring): . })' <<< "$items")
+# echo "$items" > mapped2.json
+items=$(jq 'map(to_entries)' <<< "$items")
+# echo "$items" > entries.json
+items=$(jq 'flatten(1)' <<< "$items")
+# echo "$items" > flattened.json
+items=$(jq 'group_by(.key)' <<< "$items")
+# echo "$items" > groupedByKey.json
+items=$(jq 'map(if length == 1 then .[0] else ( { key: (.[0].key), value: (map(.value ) | flatten(1)) } ) end) | from_entries' <<< "$items")
+# echo "$items" > xx.json
+
 
 echo "Outputting CSL JSON"
 echo "$items" > "$(dirname "$0")/../data/bibliography.json"
