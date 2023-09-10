bff50baaa03d60279c6f3acb4d3c46ff5ed55ade
Matt Heffron <matt.heffron@sbcglobal.net>
2023-09-09 20:11:41 -0700

    Included the Abstract information in the Bibliography, using an "expander" to show/hide it. The Notes are now in a tooltip on an icon in the "Notes" table column. (So they don't cause an unusually wide table.) Authors & Notes content with multiple entries are split more cleanly. The color and some size choices are just "working guesses". Refining them should be discussed before going "public" with this.

--- scripts/update_bibliography.sh
+++ scripts/update_bibliography.sh
@@ -32,23 +32,41 @@ add_items_from_collection $root_collection
 echo "Got $(jq '. | length' <<< "$items") items"
 
 # Piece-wise processing for debugging:
-items=$(jq 'sort_by(.data.date)' <<< "$items")
-# echo "$items" > sorted.json
-items=$(jq 'map(.csljson)' <<< "$items")
-# echo "$items" > mapped.json
+# echo "$items" > 0-rawItems.json
+items=$(jq 'map(.csljson * .data)' <<< "$items")
+# echo "$items" > 1-mapped.json
+
+items=$(jq 'map(if has("note") then .note |= sub("^<div.+?>(?<body>.*)</div>$"; "\(.body)"; "gm") else . end)' <<< "$items")
+# echo "$items" > 2-cleanDiv.json
+#find and eliminate duplicate .id entries
+groupedByID=$(jq 'group_by(.id)' <<< "$items")
+dupIDs=$(jq 'map(select(length>1) | .[0])' <<< "$groupedByID")
+# echo "$dupIDs" > dupIDs.json
+#remove duplicates
+items=$(jq 'map(.[0]) | sort_by(.date)' <<< "$groupedByID")
+# echo "$items" > 3-noDupSorted.json
+finalCount=$(jq '. | length' <<< "$items")
+# onlyURL=$(jq 'map(if ((has("url") and (.url | tostring | length)) | not) and (has("URL") and (.URL | tostring | length)) then . else empty end)' <<< "$items")
+# echo "$onlyURL" > onlyURL.json
+# consolidate URL into url key
+# consolidated=$(jq 'map(if (has("URL") and (.URL | tostring | length)) then setpath(["url"]; .URL) | del(.URL) else . end)' <<< "$items")
+# echo "$consolidated" > consolidated.json
+# noURL=$(jq 'map(if (has("url") and (.url | tostring | length >= 1)) then empty else . end) | . // empty' <<< "$items")
+# echo "$noURL" > noURL.json
 items=$(jq 'group_by(.issued."date-parts"[0][0])' <<< "$items")
-# echo "$items" > grouped.json
+# echo "$items" > 4-grouped.json
 items=$(jq 'map({ (.[0].issued."date-parts"[0][0] // "Undated" | tostring): . })' <<< "$items")
-# echo "$items" > mapped2.json
+# echo "$items" > 5-Undated.json
 items=$(jq 'map(to_entries)' <<< "$items")
-# echo "$items" > entries.json
+# echo "$items" > 6-entries.json
 items=$(jq 'flatten(1)' <<< "$items")
-# echo "$items" > flattened.json
+# echo "$items" > 7-flattened.json
 items=$(jq 'group_by(.key)' <<< "$items")
-# echo "$items" > groupedByKey.json
+# echo "$items" > 8-groupedByKey.json
 items=$(jq 'map(if length == 1 then .[0] else ( { key: (.[0].key), value: (map(.value ) | flatten(1)) } ) end) | from_entries' <<< "$items")
-# echo "$items" > xx.json
+# echo "$items" > 9-final.json
 
 
 echo "Outputting CSL JSON"
+echo "$finalCount entries"
 echo "$items" > "$(dirname "$0")/../data/bibliography.json"
