cf2fd0a83fd6729c0018a9c9d7513408f8c73d7b
Matt Heffron <matt.heffron@sbcglobal.net>
2023-10-02 23:19:53 -0700

    Refine the Bibliography layout for "better" readability. (IMHO) Get child item info from Zotero, and use that to fill in some missing urls; with potential for using other info later. Move some common or complicated jq code to bib-fns.jq for readability in update_bibliography.sh. Cleanup some processing of notes and abstract text in bibTable.html.

---
+++ scripts/bib-fns.jq
@@ -0,0 +1,72 @@
+# Module of JQ functions for manipulating JSON returned from Zotero API
+
+def nonBlankKey($keyName): 
+  has($keyName) and (.[$keyName] | tostring | length >= 1);
+
+def blankKey($keyName): 
+  (has($keyName) and (.[$keyName] | tostring | length >= 1)) | not;
+
+def unwrapDiv:
+  sub("^<div.+?>(?<body>.*)</div>$"; "\(.body)"; "gm");
+
+def moveURL_to_url:
+  select(nonBlankKey("URL")) | (setpath(["url"]; .URL) | del(.URL)) // .;
+
+def cleanAbstracts:
+  if blankKey("abstract") and nonBlankKey("abstractNote") then
+    setpath(["abstract"]; .abstractNote) | del(.abstractNote) 
+  elif blankKey("abstractNote") or (nonBlankKey("abstract") and (.abstract == .abstractNote)) then 
+    del(.abstractNote) 
+  else
+    .
+  end;
+
+def getTargetInfo:
+  {target: (.parentItem // .key)} + .;
+  
+def reconstituteGroupedEntries:  # assumes that array of all entries in a group (effectively by year) is the input
+  if length == 1 then .[0] else ( { key: (.[0].key), value: (map(.value ) | flatten(1) ) } ) end | .value |= sort_by(.title);
+  
+def updateParentUrl:
+    . as $parent 
+    | (.children | map(select(nonBlankKey("url")) | .url)) as $childurls
+    # For now, use first child url to set for blank parent url
+    | if ($childurls | length >= 1) then
+        #only 1
+        if blankKey("url") then         # parent has no url
+          . * {url: $childurls[0]}
+        else
+          .                              # For now, don't do anything here
+        end
+      else                               # For now, don't do anything here, either
+      # none or many
+        .
+      end;
+
+def applyChildrenAmendments:
+  if (.children | length < 1) then 
+    . 
+  else
+    updateParentUrl
+#    . as $parent |
+#    if (blankKey("url")) then 
+#      .children 
+#      | map(select()) as $url | .url |= $url 
+#    else 
+#      .children 
+#         | map(select()) as $url | .url |= $url 
+#      .
+#    end;
+  end;
+  
+def getNeededUrls:  # assumes that array of all current items is the input
+    map(.key) as $keys                      # extract all existing keys and keep 'em handy
+  | map(                                 # collect all of the "up" link urls and their keys
+        .links.up.href                  # get the "up" link url
+        | select(. != null)             # only if there IS an up url
+        | [(split("/")|last), .])        # make an array with the key, and the url; an entry in the collected array
+  | map(                                # for each key/url pair
+        .[0] as $k                         # get the key
+        | select(($keys | all(. != $k)))   # if this key isn't already in the $keys
+        | .[1]?)                          # include this url in this collected array
+;
--- scripts/update_bibliography.sh
+++ scripts/update_bibliography.sh
@@ -12,7 +12,7 @@ function add_items_from_collection () {
     local start=0
     local limit=100
     while :; do
-        local this_page=$(curl -s "https://api.zotero.org/groups/$GROUP_ID/collections/$collection_key/items/top?include=data,csljson&start=$start&limit=$limit&v=3")
+        local this_page=$(curl -s "https://api.zotero.org/groups/$GROUP_ID/collections/$collection_key/items?include=data,csljson&start=$start&limit=$limit&v=3")
         items=$(jq -s 'add' <<< "$this_page$items")
         start=$(($start + $limit))
 
@@ -27,32 +27,65 @@ function add_items_from_collection () {
 }
 
 root_collection="FSK5IX4F"
-add_items_from_collection $root_collection
+if [[ $# -eq 0 ]] ; then
+  add_items_from_collection $root_collection
+else
+  items=$(<$1)
+fi
+
+echo "$items" > 00-rawItems-pre-needed.json
+
+while read neededUrl; do
+    item=$(curl -s "$neededUrl?include=data,csljson&v=3")
+	items=$(jq -s 'add' <<< "[$item]$items")
+done < <(jq -r 'include "./bib-fns";getNeededUrls|.[]' <<< "$items")
+
 
 echo "Got $(jq '. | length' <<< "$items") items"
 
 # Piece-wise processing for debugging:
 # echo "$items" > 0-rawItems.json
+
 items=$(jq 'map(.csljson * .data)' <<< "$items")
 # echo "$items" > 1-mapped.json
-
-items=$(jq 'map(if has("note") then .note |= sub("^<div.+?>(?<body>.*)</div>$"; "\(.body)"; "gm") else . end)' <<< "$items")
+items=$(jq 'include "./bib-fns";map(if has("note") then .note |= unwrapDiv else . end)' <<< "$items")
 # echo "$items" > 2-cleanDiv.json
-#find and eliminate duplicate .id entries
-groupedByID=$(jq 'group_by(.id)' <<< "$items")
-dupIDs=$(jq 'map(select(length>1) | .[0])' <<< "$groupedByID")
+#find and eliminate duplicate .key entries
+groupedByKey=$(jq 'group_by(.key)' <<< "$items")
+dupIDs=$(jq 'map(select(length>1) | .[0])' <<< "$groupedByKey")
 # echo "$dupIDs" > dupIDs.json
 #remove duplicates
-items=$(jq 'map(.[0]) | sort_by(.date)' <<< "$groupedByID")
+items=$(jq 'map(.[0]) | sort_by(.date)' <<< "$groupedByKey")
 # echo "$items" > 3-noDupSorted.json
-finalCount=$(jq '. | length' <<< "$items")
-# onlyURL=$(jq 'map(if ((has("url") and (.url | tostring | length)) | not) and (has("URL") and (.URL | tostring | length)) then . else empty end)' <<< "$items")
-# echo "$onlyURL" > onlyURL.json
-# consolidate URL into url key
-items=$(jq 'map(if (has("URL") and (.URL | tostring | length)) then setpath(["url"]; .URL) | del(.URL) else . end)' <<< "$items")
+# consolidate URL into url field
+items=$(jq 'include "./bib-fns";map(if nonBlankKey("URL") then setpath(["url"]; .URL) | del(.URL) else . end)' <<< "$items")
 # echo "$items" > consolidated.json
-# noURL=$(jq 'map(if (has("url") and (.url | tostring | length >= 1)) then empty else . end) | . // empty' <<< "$items")
+
+items=$(jq 'include "./bib-fns";map(getTargetInfo)' <<< "$items")
+# echo "$items" > withTargetInfo.json
+
+# now use children items to amend the info for the parentItem
+allFixupInfo=$(jq 'group_by(.target)|map(sort_by(.parentItem))' <<< "$items")
+# echo "$allFixupInfoGrouped" > allFixupInfoGrouped.json
+# embed children into their parentItem (children field). Then delete items that Zotero indicated as such.
+items=$(jq 'map(if has(1) then (first + {children: .[1:]}) else first end)|map(select(.deleted|not))' <<< "$allFixupInfo")
+# echo "$items" > itemsNestedChildren.json
+
+# echo "Got $(jq '. | length' <<< "$items") clean, top-level items"
+items=$(jq 'include "./bib-fns";map(applyChildrenAmendments)' <<< "$items")
+# echo "$items" > updatedItems.json
+
+# absNote=$(jq 'include "./bib-fns";map(select((nonBlankKey("abstract") or nonBlankKey("abstractNote")) and (.abstract != .abstractNote)) | {key: .key, title: .title, abstract: .abstract, abstractNote: .abstractNote})' <<< "$items")
+# echo "$absNote" > absNoteMismatch.json
+# remove redundant .abstractNote fields
+items=$(jq 'include "./bib-fns";map(cleanAbstracts)' <<< "$items")
+# echo "$items" > abstractsCleaned.json
+noURL=$(jq 'include "./bib-fns";map(select(blankKey("url")))' <<< "$items")
 # echo "$noURL" > noURL.json
+finalCount=$(jq '. | length' <<< "$items")
+# Remove .children arrays, if any. Save space.
+items=$(jq 'map(del(.children))' <<< "$items")
+# Group by year
 items=$(jq 'group_by(.issued."date-parts"[0][0])' <<< "$items")
 # echo "$items" > 4-grouped.json
 items=$(jq 'map({ (.[0].issued."date-parts"[0][0] // "Undated" | tostring): . })' <<< "$items")
@@ -63,7 +96,7 @@ items=$(jq 'flatten(1)' <<< "$items")
 # echo "$items" > 7-flattened.json
 items=$(jq 'group_by(.key)' <<< "$items")
 # echo "$items" > 8-groupedByKey.json
-items=$(jq 'map(if length == 1 then .[0] else ( { key: (.[0].key), value: (map(.value ) | flatten(1)) } ) end) | from_entries' <<< "$items")
+items=$(jq 'include "./bib-fns";map(reconstituteGroupedEntries) | from_entries' <<< "$items")
 # echo "$items" > 9-final.json
 
 
